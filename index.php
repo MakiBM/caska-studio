88213521c18b00affb4893d890323eaea6efb074
Bartosz Makos <loudnownet@gmail.com>
2024-10-31 13:25:24 +0100

    section stab & global styles

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Caska Studio</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Abril+Fatface&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="global.css">
+</head>
+<body>
+  <?php
+
+    // include 'components/header.php';
+    include 'components/hero.php';
+    include 'components/cta.php';
+    include 'components/testimonials.php';
+    include 'components/tag-cloud.php';
+    include 'components/video-grid.php';
+    include 'components/logo-wall.php';
+    include 'components/contact.php';
+
+  ?>
+</body>
+</html>
